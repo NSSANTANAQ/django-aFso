4864615a60d2a4190c29e0207510d8b181cf0266
NSANTANAQ <nestorsantana12@gmail.com>
2024-11-26 11:53:47 -0500

    Prueba

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,28 +82,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ["PGDATABASE"],
-#         'USER': os.environ["PGUSER"],
-#         'PASSWORD': os.environ["PGPASSWORD"],
-#         'HOST': os.environ["PGHOST"],
-#         'PORT': os.environ["PGPORT"],
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sistema',
-        'HOST': "autorack.proxy.rlwy.net",
-        'PORT': '20857',
-        'USER': "root",
-        'PASSWORD': "VeMwJCeMN2rjpI0O.~k7cFj29.tEK0Vb",
-    },
-
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["PGDATABASE"],
+        'USER': os.environ["PGUSER"],
+        'PASSWORD': os.environ["PGPASSWORD"],
+        'HOST': os.environ["PGHOST"],
+        'PORT': os.environ["PGPORT"],
+    }
 }
 
 
