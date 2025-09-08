2cf2a9717c61188341a1e71157f48aad5e968b54
NSANTANAQ <nestorsantana12@gmail.com>
2025-09-08 11:10:45 -0500

    Prueba

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 AUTH_USER_MODEL = "autenticacion.Usuario"
 
-# ALLOWED_HOSTS = ["servicioinventario.up.railway.app"]
+ALLOWED_HOSTS = ["servicioinventario.up.railway.app"]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://servicioinventario.up.railway.app',
